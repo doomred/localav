f721a9f026b3249463b9daec7c2d26937bb00649
dye E. jarhoo <gene0mega@zoho.com>
2014-08-15 10:51:52 +0800

    Working grabbable dock, update misc -dye
    The docker.js and grabber.js is now working.These pieces of js code
    should be assembled into one portable functional-bookmark.
    The remaining block is fetch.js and maybe some css modifiction.

--- docker.js
+++ docker.js
@@ -10,46 +10,85 @@
 /* some codes to gather json */
 /* and it's storged into data */
 /* it's now faked */
-var data = {"code":200,"message":"request success.","success":true,"result":{"C20":{"quality":"\u9ad8\u6e05","totalseconds":252,"totalbytes":0,"files":[{"type":"flv","seconds":252,"bytes":0,"url":"http:\/\/edge.v.iask.com.lxdns.com\/19951758.flv?KID=sina,viask&Expires=1408118400&ssig=MFYELPrmCG","no":0}]},"C10":{"quality":"\u9ad8\u6e05","totalseconds":252,"totalbytes":0,"files":[{"type":"flv","seconds":252,"bytes":0,"url":"http:\/\/edge.v.iask.com.lxdns.com\/19951758.flv?KID=sina,viask&Expires=1408118400&ssig=MFYELPrmCG","no":0}]}}}
+var data = {"code":200,"message":"request success.","success":true,"result":{"C20":{"quality":"\u9ad8\u6e05","totalseconds":252,"totalbytes":0,"files":[{"type":"flv","seconds":252,"bytes":100000000,"url":"http:\/\/edge.v.iask.com.lxdns.com\/19951758.flv?KID=sina,viask&Expires=1408118400&ssig=MFYELPrmCG","no":0}]},"C10":{"quality":"\u9ad8\u6e05","totalseconds":252,"totalbytes":0,"files":[{"type":"flv","seconds":252,"bytes":100000000,"url":"http:\/\/edge.v.iask.com.lxdns.com\/19951758.flv?KID=sina,viask&Expires=1408118400&ssig=MFYELPrmCG","no":0}]}}}
+
+
+
 
 
 var body = document.body;
 var floatbox = document.createElement('div');
 floatbox.style.top = '64px';
 floatbox.style.left = '64px';
+// floatbox.style.width = '128px';
 floatbox.style.position = 'absolute';
 floatbox.style.backgroundColor = 'red';
-floatbox.style.
 floatbox.id = 'box';
 
-var contentbox = document.createElement('div');
-var breakrow = document.createElement('br');
 
+
+
+function loop(obj) {
+    for( var key in obj ) {
+        if( obj.hasOwnProperty(key) ) {
+
+var contentbox = document.createElement('div');
+    
 contentbox.class = 'contentbox';
-contentbox.style.margin = '1em 2em'
-contentbox.appendChild(document.createTextNode(data.result.C10.quality + '[' + data.result.C10.files.type + ']'));
-contentbox.appendChild(breakrow);
-contentbox.appendChild(document.createTextNode(data.result.C10.files.bytes / 1024 / 1024 + "MB"));
+contentbox.style.padding = '1em 2em';
+            
+  /* problem next line */
+var avSize = parseInt(obj[key].files[0].bytes / 1024 / 1024);
+//contentbox.style.float = 'left';
+contentbox.appendChild(document.createTextNode(obj[key].quality + '[' + obj[key].files[0].type + ']'));
+contentbox.innerHTML += "<br />";
+contentbox.appendChild(document.createTextNode(avSize + "MB"));
+contentbox.onclick = function () {                       /* function to download withing with pure js, midified via S.O. */
+    var url = obj[key].files[0].url;
+    var ranNum3 = parseInt(Math.random() * 1000);
+    var hiddenIFrameID = 'hiddenDownloader' + ranNum3;   /* random ID for dump browser */
+
+        iframe = document.getElementById(hiddenIFrameID);
+    if (iframe === null) {
+        iframe = document.createElement('iframe');
+        iframe.id = hiddenIFrameID;
+        iframe.style.display = 'none';
+        iframe.style.position = 'absoulte';
+        iframe.style.top = '0px';
+        iframe.style.left = '0px';
+        document.body.appendChild(iframe);
+    }
+    iframe.src = url;
+};
+            
 floatbox.appendChild(contentbox);
 
-// if exist C20
-var contentbox2 = document.createElement('div');
+        }
+    }
 
-contentbox.class = 'contentbox';
-contentbox.style.margin = '1em 2em'
-contentbox2.appendChild(document.createTextNode(data.result.C20.quality + '[' + data.result.C20.files.type + ']'));
-contentbox2.appendChild(breakrow);
-contentbox2.appendChild(document.createTextNode(data.result.C20.files.bytes / 1024 / 1024 + "MB"));
-floatbox.appendChild(contentbox2);
+};
+
+loop(data.result)
 
 
 
 
+var close = document.createElement('div');
+close.innerHTML += 'CLOSE!';
+close.onclick = function() {
+  floatbox.style.display = 'none';
+}
+floatbox.appendChild(close);
 
 
-
-
+/* clearboth block
+var clearboth = document.createElement('div');
+clearboth.style.clear = 'both';
+floatbox.appendChild(clearboth);
+ */
 
 body.appendChild(floatbox);
+
+
 
 
---
+++ grabber.js
@@ -0,0 +1,69 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+/* box Init */
+var body = document.body;
+var floatbox = document.createElement('div');
+floatbox.style.top = '64px';
+floatbox.style.left = '64px';
+floatbox.style.width = '128px';
+floatbox.style.position = 'absolute';
+floatbox.style.backgroundColor = 'red';
+floatbox.id = 'box';
+/* append fixed hight to become visible */
+floatbox.style.height = '128px';
+body.appendChild(floatbox);
+
+/* finished Init */
+
+
+/* Finish rending, start enable grabbing */
+function grabbing() {     /* need something instead of window.onload */
+  var grabber = document.getElementById('box');
+
+  document.onmousedown = coordinates;
+  document.onmouseup = mouseup;
+
+  function coordinates(e) {
+
+    if (e == null) { e = window.event;}
+    
+    // e.srcElement holds the target element in IE, whereas e.target holds the target element in Firefox
+    // Both properties return the HTML element the event took place on.
+
+    var sender = (typeof( window.event ) != "undefined" ) ? e.srcElement : e.target;
+          // alert(sender.id);
+
+    if (sender.id=="box") {
+      
+      mouseover = true;
+      pleft = parseInt(grabber.style.left);
+      ptop = parseInt(grabber.style.top);
+      xcoor = e.clientX;
+      ycoor = e.clientY;
+      document.onmousemove = moveBox;
+      return false;
+    } 
+    return false;
+  }
+
+  function moveBox(e) {
+    if (e == null) { e = window.event; }
+    grabber.style.left = pleft+e.clientX-xcoor+"px";
+    grabber.style.top = ptop+e.clientY-ycoor+"px";
+    return false;
+  }
+
+  function mouseup(e) {
+    document.onmousemove = null;
+  }
+}
+
+
+  grabbing();
